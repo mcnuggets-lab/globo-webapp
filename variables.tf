7012fdf9007791dadd195274126a43b77870a635
Kit-Ho Mak <mcnuggets23@gmail.com>
2024-09-15 19:01:46 +0800

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,65 @@
+
+variable "region" {
+  type        = string
+  description = "(Optional) AWS Region to deploy in. Defaults to us-east-1."
+  default     = "us-east-1"
+}
+
+variable "aws_access_key_id" {
+  type        = string
+  description = "AWS_ACCESS_KEY_ID"
+}
+
+variable "aws_secret_access_key" {
+  type        = string
+  description = "AWS_SECRET_ACCESS_KEY"
+}
+
+variable "aws_session_token" {
+  type        = string
+  description = "AWS_SESSION_TOKEN"
+}
+
+variable "prefix" {
+  type        = string
+  description = "(Required) Prefix to use for all resources in this module."
+}
+
+variable "environment" {
+  type        = string
+  description = "(Required) Environment of all resources"
+}
+
+variable "billing_code" {
+  type        = string
+  description = "(Required) Billing code for network resources"
+}
+
+# Application variables
+
+variable "ip_range" {
+  default = "0.0.0.0/0"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "(Optional) EC2 Instance type to use for web app. Defaults to t3.micro."
+  default     = "t2.micro"
+}
+
+variable "api_key" {
+  type        = string
+  description = "(Required) API key for web app to talk to SaaS platform."
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "(Required) List of subnet IDs for EC2 instance deployments."
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "(Required) VPC ID of VPC for application deployment."
+}
+
+
